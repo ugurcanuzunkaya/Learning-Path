6153e7143fa9569eb06b45b9b39e8b11fcc21ed0
ugurcan <ugurcanuzunkaya1@gmail.com>
2022-07-04 23:20:05 +0300

    v0.10
    Finished the processing data topic. Done guided project 4. Lastly, add some notes about numpy to notes.

---
+++ GlobalAIHub/Python_Bootcamp/Projeler/proje1.py
@@ -0,0 +1,10 @@
+"""
+Information about Project
+Course is Mathematics. Grade letters; 100-90: A, 89-70: B, 69-60: C, 59-50: D, 49-0: F.
+Program takes student name, school no and exam grade. It keeps the information after entry.
+Program shows if student passed or not and prints "Passed" or "Not Passed".
+Program creates a dataframe for passed and not passed students and export to excel.
+"""
+
+import pandas as pd
+
